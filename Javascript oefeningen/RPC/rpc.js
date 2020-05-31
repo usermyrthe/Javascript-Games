9d60c411262cb27bccc03053f43484e3daaba476
usermyrthe <56116163+usermyrthe@users.noreply.github.com>
2020-05-31 21:32:46 +0200

    Oefeningen

---
+++ Javascript oefeningen/RPC/rpc.js
@@ -0,0 +1,76 @@
+function rpsGame(yourChoice){
+    console.log(yourChoice);
+
+    var humanChoice, botChoice;
+    humanChoice = yourChoice.id;
+
+    botChoice = numberToChoice(randToRpsInt());
+    console.log('Computer choice:', botChoice);
+
+    results = decideWinner(humanChoice, botChoice);
+    console.log(results);
+
+    message = finalMessage(results);
+    console.log(message);
+    rpsFrontEnd(yourChoice.id, botChoice, message);
+    
+}
+function randToRpsInt(){
+    return Math.floor(Math.random() * 3);
+}
+
+function numberToChoice(number){
+    return ['rock', 'paper', 'scissors'][number];
+}
+
+function decideWinner(yourChoice,computerChoice){
+    var rpsDatabase = {
+        'rock':{'scissors': 1, 'rock': 0.5, 'paper': 0},
+        'paper':{'rock': 1, 'paper': 0.5, 'scissors': 0},
+        'scissors':{'paper': 1, 'scissors': 0.5, 'rock': 0}
+
+    };
+
+    var yourScore = rpsDatabase[yourChoice][computerChoice];
+    var computerScore = rpsDatabase[computerChoice] [yourChoice];
+
+    return [yourScore, computerScore];
+
+}
+
+function finalMessage([yourScore, computerScore]) {
+    if (yourScore === 0) {
+        return{'message': 'You lost!', 'color': 'red'};
+    } else if (yourScore === 0.5){
+      return{'message': 'You tied!', 'color': 'yellow'};
+    } else{
+      return{'message': 'You won!', 'color': 'green'};  
+    }    
+
+        }
+
+        function rpsFrontEnd(humanImageChoice, botImageChoice, finalMessage){
+            var imagesDatabase = {
+                'rock': document.getElementById('rock').src,
+                'paper': document.getElementById('paper').src,
+                'scissors': document.getElementById('scissors').src
+            }
+
+            document.getElementById('rock').remove();
+            document.getElementById('paper').remove();
+            document.getElementById('scissors').remove();
+
+            var humanDiv = doument.createElement('div');
+            var botDiv = document.createElement('div');
+            var messageDiv = document.createElement('div');
+
+            humanDiv.innerHTML = "<img src='" + imagesDatabase[humanImageChoice] + "' height=150 width=150 style='box-shadow: 0px 10px 50px rgba(37, 50, 233, 1);'>"
+            messageDiv.innerHTML = <humanImageChoice1 style='color: "+ finalMessage['color']
+            botDiv.innerHTML = "<img src='" + imagesDatabase[botImageChoice] + "' height=150 width=150 style='box-shadow: 0px 10px 50px rgba(243, 38, 24, 1);'>"
+            
+            
+        document.getElementById('flex-box-rps-div').appendChild(humanDiv);
+        document.getElementById('flex-box-rps-div').appendChild(botDiv);
+        }
+    
+
